0cdb0ac6ab5ca615e290ea4f4ce8deea8d274431
tofisvej <svejovsky@spaceknow.com>
2025-02-04 01:11:16 +0100

    fuck

--- content.js
+++ content.js
@@ -1,35 +1,22 @@
-const siteRules = {
-    "youtube.com": [
-        "#end",
-        "#logo-icon",
-    ],,
-    "songsterr.com": [
-        "#showroom_panel", 
-        "#Redesign2024_ListPages_ATF_leaderboard_728x90",
-        "#menu-plus",
-        "#showroom",
-        "#Redesign_ATF_tab_page_728x90",
-        "#text-showroom",
-        ".rq25k",
-        "#hide-layer-overlay",
-        ".Cip2pk",
-        ".Cuqdk",
-        ".Cto1rx",
-        ".D5an6"
-    ]
-};
-
-function getCurrentDomain() {
-    return window.location.hostname.replace("www.", "");
-}
+const elementsToRemove = [
+    "#showroom_panel", 
+    "#Redesign2024_ListPages_ATF_leaderboard_728x90",
+    "#menu-plus",
+    "#showroom",
+    "#Redesign_ATF_tab_page_728x90",
+    "#text-showroom",
+    ".rq25k",
+    "#hide-layer-overlay",
+    ".Cip2pk",
+    ".Cuqdk",
+    ".Cto1rx",
+    ".D5an6"
+];
 
 function removeElements() {
-    const domain = getCurrentDomain();
-    if (siteRules[domain]) {
-        siteRules[domain].forEach(selector => {
-            document.querySelectorAll(selector).forEach(el => el.remove());
-        });
-    }
+    elementsToRemove.forEach(selector => {
+        document.querySelectorAll(selector).forEach(el => el.remove());
+    });
 }
 
 removeElements();
